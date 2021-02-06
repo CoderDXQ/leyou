eac8af550ceb20220ef00f1f4f6f7af186bb7194
cococolabear <794055465@qq.com>
2021-02-07 02:11:08 +0800

    商品品牌查询，这个功能是为商品新增功能服务的

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -1,9 +1,13 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.pojo.Brand;
+import jdk.internal.dynalink.linker.LinkerServices;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * @author Duan Xiangqing
@@ -17,4 +21,7 @@ public interface BrandMapper extends Mapper<Brand> {
 
     @Insert("INSERT INTO tb_category_brand (category_id, brand_id) VALUES (#{cid},#{bid})")
     int insertCategoryAndBrand(@Param("cid") Long cid, @Param("bid") Long bid);
+
+    @Select("SELECT * FROM tb_brand a INNER JOIN tb_category_brand b on a.id=b.brand_id where b.category_id=#{cid}")
+    List<Brand> selectBrandsByCid(Long cid);
 }
