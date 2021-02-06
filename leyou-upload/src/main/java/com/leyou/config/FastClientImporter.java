52b67685830004b65114227a3f1ef6e5c29b084f
cococolabear <794055465@qq.com>
2021-02-06 22:08:09 +0800

    使用FastDFS实现上传文件功能

---
+++ leyou-upload/src/main/java/com/leyou/config/FastClientImporter.java
@@ -0,0 +1,18 @@
+package com.leyou.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/2/6 8:55 下午
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastClientImporter {
+}
--- leyou-upload/src/main/java/com/leyou/service/UploadService.java
+++ leyou-upload/src/main/java/com/leyou/service/UploadService.java
@@ -1,7 +1,11 @@
 package com.leyou.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,6 +31,9 @@ public class UploadService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
 
+    @Autowired //注入
+    private FastFileStorageClient storageClient;
+
     public String uploadImage(MultipartFile file) throws IOException {
 
         String originalFilename = file.getOriginalFilename();
@@ -38,31 +45,43 @@ public class UploadService {
             return null;
         }
 
-//        校验文件内容
-        BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
-        if (bufferedImage == null) {
-            LOGGER.info("文件内容不合法：{}", originalFilename);
-            return null;
-        }
-
         try {
+//        校验文件内容
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+            if (bufferedImage == null) {
+                LOGGER.info("文件内容不合法：{}", originalFilename);
+                return null;
+            }
+
+
 //        保存到服务器
+            /*
             File getpath = new File("..");
 
             String savepath = getpath.getCanonicalPath() + "/image/" + originalFilename;
             System.out.println(savepath);
             file.transferTo(new File(savepath));
+             */
+
+//            分离出上传文件的格式
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+//            返回文件上传到FastDFS后的存储路径
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
 
 
 //        需要配置nginx和hosts  http://image.leyou.com/upload/实际上就是本地的一个文件夹
             //        返回url 进行回显
-            String url = "http://image.leyou.com/" + file.getOriginalFilename();
+//            String url = "http://image.leyou.com/" + file.getOriginalFilename();
+
+            String url = "http://image.leyou.com/" + storePath.getFullPath();
+            System.out.println(url);
+
+            return url;
         } catch (Exception e) {
             LOGGER.info("服务器内部错误：{}" + originalFilename);
             e.printStackTrace();
+            return null;
         }
-
-        return null;
 
     }
 
