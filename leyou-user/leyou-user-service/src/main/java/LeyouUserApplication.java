54727277ada02be6af48f363144e3fe7e7cb1ab3
cococolabear <794055465@qq.com>
2021-02-17 20:40:08 +0800

    搭建用户微服务工程

---
+++ leyou-user/leyou-user-service/src/main/java/LeyouUserApplication.java
@@ -0,0 +1,21 @@
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/2/17 8:35 下午
+ */
+
+//???路径要记得更改
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.leyou.user.mapper")
+public class LeyouUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouUserApplication.class, args);
+    }
+}
