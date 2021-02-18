18f8339b98f567c041ad93243b294b96208c2b32
cococolabear <794055465@qq.com>
2021-02-19 01:39:42 +0800

    添加拦截器解析用户信息

---
+++ leyou-cart/src/main/java/com/leyou/cart/config/LeyouWebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.leyou.cart.config;
+
+import com.leyou.cart.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/2/19 1:29 上午
+ */
+
+@Configuration
+public class LeyouWebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");//拦截所有路径
+
+    }
+}
