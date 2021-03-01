986d589d17ca0d25796eab39391172baf671f7ee
cococolabear <794055465@qq.com>
2021-03-01 16:22:12 +0800

    秒杀测试、

---
+++ leyou-auth/leyou-auth-service/src/test/java/TokenTest.java
@@ -0,0 +1,50 @@
+import com.leyou.LeyouAuthApplication;
+import com.leyou.auth.service.AuthService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/1 3:35 下午
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LeyouAuthApplication.class)
+public class TokenTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    public void getTokenCSV() throws IOException {
+
+
+        BufferedWriter bw = null;
+        try {
+//            ？？？这个地址需要视情况而变化
+            File csv = new File("/Users/duanxiangqing/Desktop/leyoucode/leyou/others/Token.csv");
+            bw = new BufferedWriter(new FileWriter(csv, true));
+            for (int i = 0; i < 5000; i++) {
+                //            两个参数分别是用户名和密码
+                String token = this.authService.accredit("username" + i, "abcdefg" + i);
+                bw.write("username" + i + "," + token);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bw.close();
+        }
+
+
+    }
+}
--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/utils/CodecUtils.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/utils/CodecUtils.java
@@ -2,10 +2,20 @@ package com.leyou.user.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.UUID;
 
 public class CodecUtils {
+
+    public static String passwordBcryptEncode(String username, String password) {
+
+        return new BCryptPasswordEncoder().encode(username + password);
+    }
+
+    public static Boolean passwordConfirm(String rawPassword, String encodePassword) {
+        return new BCryptPasswordEncoder().matches(rawPassword, encodePassword);
+    }
 
     public static String md5Hex(String data, String salt) {
         if (StringUtils.isBlank(salt)) {
