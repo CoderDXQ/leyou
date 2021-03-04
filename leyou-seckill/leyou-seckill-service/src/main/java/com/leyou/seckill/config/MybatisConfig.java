854f42d7eec78f376625c056c90d1baf10fe8baf
cococolabear <794055465@qq.com>
2021-03-04 23:57:16 +0800

    解决启动报错

---
+++ leyou-seckill/leyou-seckill-service/src/main/java/com/leyou/seckill/config/MybatisConfig.java
@@ -0,0 +1,46 @@
+package com.leyou.seckill.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/4 11:49 下午
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        System.out.println(dataSourceProperties.getUrl());
+        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+
+        return dataSource;
+
+    }
+
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
+
